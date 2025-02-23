d827f34a2d09bea17457a941eb5e83c8c8d9a904
nookteanook111 <150282612+nookteanook111@users.noreply.github.com>
2025-02-23 09:22:46 +0700

    done bottom nav

--- components/layout/bottom-nav.tsx
+++ components/layout/bottom-nav.tsx
@@ -14,9 +14,12 @@ export function BottomNav() {
   ];
 
   return (
-    <div className="fixed bottom-4 left-1/2 z-50 -translate-x-1/2 px-4 md:hidden">
+    <div className="fixed bottom-4 left-1/2 z-50 -translate-x-1/2 px-4">
       <div className="mx-auto w-fit">
-        <ExpandableTabs tabs={tabs} />
+        <ExpandableTabs 
+          tabs={tabs} 
+          className="max-w-[95vw] sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px]"
+        />
       </div>
     </div>
   );
