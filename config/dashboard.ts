6b0789a8782361e5608bc977dafb4d0e68660f31
nookteanook111 <59superrichsol@gmail.com>
2025-03-06 21:29:50 +0700

    done

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -45,8 +45,8 @@ export const sidebarLinks: SidebarNavItem[] = [
     title: "OPTIONS",
     items: [
       { href: "/dashboard/settings", icon: "settings", title: "Settings" },
-      { href: "/", icon: "home", title: "Homepage" },
-      { href: "/docs", icon: "bookOpen", title: "Documentation" },
+     
+      
       {
         href: "#",
         icon: "messages",
