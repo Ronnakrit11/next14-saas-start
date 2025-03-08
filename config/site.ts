d4f5c9c4f68d655b56abd9c189a4f1956aaa88a0
nookteanook111 <59superrichsol@gmail.com>
2025-03-08 22:44:25 +0700

    seo

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
   name: "FairFlows",
-  description: "FairFlows - ระบบจัดการการจ้างงานที่ปลอดภัย ทั้งผู้จ้างเเละผู้รับจ้าง ด้วยระบบ Escrow ที่มีความน่าเชื่อถือ",
+  description: "FairFlows - ตัวกลางการจ้างงานที่ปลอดภัย ทั้งผู้จ้างเเละผู้รับจ้าง ด้วยระบบ Escrow ที่มีความน่าเชื่อถือ",
   url: site_url,
   ogImage: `${site_url}/fairflows.jpg`,
   links: {
